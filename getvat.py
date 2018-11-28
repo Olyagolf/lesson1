000482fd8fda30b76c690d5cacab9aebface0c54
Olya Golf <olya.golf@gmail.com>
2018-11-28 15:56:01 +0300

    Первая версия

---
+++ getvat.py
@@ -0,0 +1,15 @@
+def get_vat(payment, percent=18):
+    try:
+        payment=float(payment)
+        percent=int(percent)
+        vat=payment/100*percent
+
+        vat=round(vat, 2)
+        return "Сумма НДС: {}".format(vat)
+    except (TypeError, ValueError):
+        return ("Не могу посчитать, проверьте вводимые значения")
+
+result=get_vat(400, "9")
+print(result)
+
+
